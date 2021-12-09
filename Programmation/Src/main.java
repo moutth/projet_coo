02b609e1b9d8240d8a569acd91ea42447f372114
Matthias <75242607+moutth@users.noreply.github.com>
2021-12-09 11:56:11 +0100

    Update main.java

--- Programmation/Src/main.java
+++ Programmation/Src/main.java
@@ -1,23 +1,18 @@
 import java.io.IOException;
-
-import controller.ComSystem;
 import controller.Controller;
 import model.*;
-
 
 public class main {
 
 	public static void main(String[] args) throws IOException {
 		
-		
 		Model model = new Model();
 		Controller controller = new Controller(model);
-		MsgSystem msg = new MsgSystem("newConnexion");
-		controller.comSystem.SendUdp("255.255.255.255", ComSystem.SERVSYST, msg);
 		
+		MsgSystem msg = new model.MsgSystem("newConnexion");
 		
-
-
+		//controller.comSystem.SendUdp("127.0.0.255", controller.comSystem.SERVSYST, msg);
+		while(true);
 	}
 
 }
