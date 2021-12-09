2de4c6b09dceb7087a32d0f8d3cea1a50f277918
Matthias <75242607+moutth@users.noreply.github.com>
2021-12-09 11:49:11 +0100

    Update ServSystem.java

--- Programmation/Src/controller/ServSystem.java
+++ Programmation/Src/controller/ServSystem.java
@@ -1,52 +1,65 @@
 package controller;
+
 import java.io.IOException;
+import model.*;
 import java.net.*;
 
 public class ServSystem extends Thread {
 
 	private static final int BUFFSIZE = 256;
-	
+
 	private ComSystem comSystem;
-	
-	ServSystem(ComSystem in){
+
+	ServSystem(ComSystem in) {
 		comSystem = in;
-		start();	//runs the thread once instanciated
+		start(); // runs the thread once instanciated
 	}
 
-	public void run(){
+	public void run() {
 		DatagramSocket socket = null;
 		try {
 			socket = new DatagramSocket(ComSystem.SERVSYST);
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
-		
+
 		byte[] buffer = new byte[BUFFSIZE];
-		
-		DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
-		
-		try {
-			socket.receive(inPacket);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		InetAddress clientAddress = inPacket.getAddress();
-		
-		String message = new String(inPacket.getData(), 0, inPacket.getLength());
-		System.out.println(message);
-		
-		switch (message) {
-			case "newConnexion":
-				String response = Integer.toString(comSystem.controller.model.currentUser.userID) + comSystem.controller.model.currentUser.pseudo;
-				DatagramPacket outPacket = new DatagramPacket(response.getBytes(), response.length(), clientAddress, ComSystem.SERVSYST);
-				try {
-					socket.send(outPacket);
-				} catch (IOException e) {
-					e.printStackTrace();
+
+		while (true) {
+			DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+
+			try {
+				socket.receive(inPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			InetAddress clientAddress = inPacket.getAddress();
+
+			if (clientAddress.toString() != "/" + comSystem.controller.model.currentUser.ip) {
+
+				String message = new String(inPacket.getData(), 0, inPacket.getLength());
+				System.out.println(message);
+
+				switch (message) {
+
+				case "newConnexion":
+					System.out.println(clientAddress);
+					String response = Integer.toString(comSystem.controller.model.currentUser.userID) + " "
+							+ comSystem.controller.model.currentUser.pseudo;
+
+					try {
+						comSystem.SendUdp(clientAddress, ComSystem.SERVSYST, new MsgSystem(response));
+						System.out.println(response);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					break;
+
+				default:
+					System.out.println("default");
 				}
-			
-			default : System.out.println(message);
+			}
 		}
 
 	}
